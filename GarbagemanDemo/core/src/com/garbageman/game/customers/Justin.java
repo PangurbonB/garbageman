40e2d29ec273bb5d2bfab1f6f6bd9c246aa22237
PangurbonB <tigersoftheforest@gmail.com>
2018-05-23 12:46:07 -0700

    f

---
+++ GarbagemanDemo/core/src/com/garbageman/game/customers/Justin.java
@@ -0,0 +1,16 @@
+package com.garbageman.game.customers;
+
+/**
+ * Created by bzonick5979 on 5/23/2018.
+ */
+
+public class Justin extends Customer{
+
+    {
+        fileName = "customer0";
+        customerName = "Justin";
+        LOCAL_MIN = LOCAL_MIN;
+        LOCAL_MAX = NOT_PICKY;
+    }
+
+}
